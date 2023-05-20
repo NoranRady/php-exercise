921a6cc4d304b40c5b57ad9fc8b7bc0a5c895a13
NoranRady <noran.rady@maxab.io>
2023-05-20 04:23:21 +0300

    Add prices chart to the historics page

--- resources/views/applications/historics.blade.php
+++ resources/views/applications/historics.blade.php
@@ -14,11 +14,13 @@
             </tr>
         </thead>
         <tbody>
-
         </tbody>
 
     </table>
 
+    <canvas id="chart"></canvas>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const symbol = '{{ $symbol }}'; // assuming the $symbol variable is passed from the controller
         fetch(`https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data?symbol=${symbol}`, {
@@ -31,9 +33,60 @@
             .then(data => {
                 const historicalData = data.prices;
                 console.log(historicalData);
+
+                // Create arrays of dates, opens, and closes
+                const dates = historicalData.map(data => data.date);
+                const opens = historicalData.map(data => data.open);
+                const closes = historicalData.map(data => data.close);
+
+                // Create chart data
+                const chartData = {
+                    labels: dates,
+                    datasets: [{
+                            label: 'Open',
+                            data: opens,
+                            borderColor: 'rgb(255, 99, 132)',
+                            fill: false,
+                        },
+                        {
+                            label: 'Close',
+                            data: closes,
+                            borderColor: 'rgb(54, 162, 235)',
+                            fill: false,
+                        },
+                    ]
+                };
+
+                // Create chart options
+                const chartOptions = {
+                    responsive: true,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Open and Close Prices',
+                        },
+                    },
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'Date',
+                            },
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'Price',
+                            },
+                            suggestedMin: Math.min(...opens, ...closes),
+                            suggestedMax: Math.max(...opens, ...closes),
+                        },
+                    },
+                };
+
+                // Get tabledata and render it to the page
                 const tableBody = document.querySelector('tbody');
                 tableBody.innerHTML = '';
-
                 historicalData.forEach(data => {
                     const row = document.createElement('tr');
                     const dateCell = document.createElement('td');
@@ -58,6 +111,14 @@
                     row.appendChild(volumeCell);
 
                     tableBody.appendChild(row);
+                });
+
+                // Create chart
+                const ctx = document.getElementById('chart').getContext('2d');
+                const chart = new Chart(ctx, {
+                    type: 'line',
+                    data: chartData,
+                    options: chartOptions,
                 });
             })
             .catch(error => console.error(error));
